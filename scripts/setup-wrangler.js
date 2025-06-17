884c0e4d90e5c032924558c26f074a79e9c750a1
Siroshun09 <siro.shun8@gmail.com>
2025-06-17 23:44:58 +0900

    chore: update biome to v2

--- scripts/setup-wrangler.js
+++ scripts/setup-wrangler.js
@@ -4,18 +4,18 @@ const projectName = process.env.VITE_PROJECT_NAME.toLowerCase();
 const customDomain = process.env.VITE_CUSTOM_DOMAIN;
 
 const wranglerConfig = {
-  name: `${projectName}-javadoc`,
-  compatibility_date: "2025-04-01",
-  workers_dev: false,
-  assets: {
-    directory: "./dist",
-  },
-  routes: [
-    {
-      pattern: `${projectName}.${customDomain}`,
-      custom_domain: true
-    }
-  ]
-}
+	name: `${projectName}-javadoc`,
+	compatibility_date: "2025-04-01",
+	workers_dev: false,
+	assets: {
+		directory: "./dist",
+	},
+	routes: [
+		{
+			pattern: `${projectName}.${customDomain}`,
+			custom_domain: true,
+		},
+	],
+};
 
 fs.writeFileSync("wrangler.json", JSON.stringify(wranglerConfig, null, 2));
